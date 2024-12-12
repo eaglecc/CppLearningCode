b1756bca1923415274cef2dd6549131ac0dd4c6e
ChengYuchao <3414906693@qq.com>
2024-12-12 21:00:03 +0800

    cstring学习

---
+++ test_cpp/CStringTest.cpp
@@ -0,0 +1,60 @@
+#include <cstring>
+#include <iostream>
+
+#pragma warning(disable:4996)
+
+// 自定义字符复制函数，将src复制到dest中，返回dest
+char* my_strcpy(char* dest, const char* src) {
+    // 使用一个指针遍历源字符串
+    char* original_dest = dest; // 保存目标字符串的起始地址
+
+    while (*src != '\0') { // 当源字符不是结束符
+        *dest = *src;       // 复制字符
+        dest++;             // 移动目标指针
+        src++;              // 移动源指针
+    }
+    *dest = '\0'; // 在目标字符串末尾添加结束符
+
+    return original_dest; // 返回目标字符串的起始地址
+}
+
+// 自定义字符串拼接函数，将src拼接到dest后面，返回dest
+char* my_strcat(char *dest, const char* src) {
+    char* original_dest = dest;
+    // 遍历到dest的末尾
+    while (*dest != '\0') {
+        dest++;
+    }
+    while (*src != '\0') {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+    return original_dest;
+}
+
+int main() {
+    char str1[20] = "Hello ";
+    const char* str2 = "World";
+    //char* res = strcat(str1, str2); // 字符串拼接
+    //std::cout << "strcat字符串拼接结果：" << res << std::endl;
+
+    //strcat_s(str1, sizeof(str1), str2);
+    //std::cout << str1 << std::endl;
+
+    //strcpy(str1 + strlen(str1), str2);
+    //std::cout << "strcpy字符串复制实现拼接结果：" << str1 << std::endl;
+
+    //my_strcpy(str1 + strlen(str1), str2);
+    //std::cout << str1 << std::endl;
+
+    //my_strcat(str1, str2);
+    //std::cout << str1 << std::endl;
+
+    char msg[] = { 'a','b','c','\0' };
+    std::cout << strlen(msg) << std::endl;
+
+    return 0;
+}
+
--- test_cpp/FileOperator.cpp
+++ test_cpp/FileOperator.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-int main() {
-    // 2. 创建流式对象
-    ofstream ofs;
-    // 3. 打开文件
-    ofs.open("test.txt", ios::app);  //当没有指定某盘路径时，创建的文件在该项目文件的路径下
-    // 4. 写入文件
-    ofs << "姓名：张三" << endl;
-    ofs << "性别：男" << endl;
-    // 5. 关闭文件
-    ofs.close();
-
-    return 0;
-}
+//int main() {
+//    // 2. 创建流式对象
+//    ofstream ofs;
+//    // 3. 打开文件
+//    ofs.open("test.txt", ios::app);  //当没有指定某盘路径时，创建的文件在该项目文件的路径下
+//    // 4. 写入文件
+//    ofs << "姓名：张三" << endl;
+//    ofs << "性别：男" << endl;
+//    // 5. 关闭文件
+//    ofs.close();
+//
+//    return 0;
+//}
